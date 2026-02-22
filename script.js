cfd960f136875d84f0731f1ebb6cd5197cccb21c
b33456386-cmd <b33456386@gmail.com>
2026-02-22 22:22:39 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
-function toggleCountry(country) {
-  const all = document.querySelectorAll(".config-list");
+fetch("data.json")
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById("countries");
 
-  all.forEach(el => {
-    if (el.id === country) {
-      el.style.display = el.style.display === "block" ? "none" : "block";
-    } else {
-      el.style.display = "none";
-    }
+    const iranTime = new Date(data.last_update).toLocaleString("fa-IR", {
+      timeZone: "Asia/Tehran",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+
+    document.getElementById("update").innerText =
+      "⏱ آخرین آپدیت: " + iranTime;
+
+    data.countries.forEach(country => {
+      const btn = document.createElement("button");
+      btn.className = "country-btn";
+      btn.innerHTML = `${country.flag} ${country.name} (${country.configs.length})`;
+
+      const list = document.createElement("div");
+      list.className = "config-list";
+
+      country.configs.forEach(cfg => {
+        const item = document.createElement("div");
+        item.className = "config-item";
+
+        item.innerHTML = `
+          <span>V2raying_config</span>
+          <button onclick="copyConfig('${cfg}')">📋</button>
+        `;
+
+        list.appendChild(item);
+      });
+
+      btn.onclick = () => {
+        const all = document.querySelectorAll(".config-list");
+        all.forEach(el => (el.style.display = "none"));
+        list.style.display =
+          list.style.display === "block" ? "none" : "block";
+      };
+
+      container.appendChild(btn);
+      container.appendChild(list);
+    });
   });
-}
 
 function copyConfig(config) {
   navigator.clipboard.writeText(config);
